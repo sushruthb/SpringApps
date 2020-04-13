336f17b00cee36a9824d37a2f9e7eca70c135b03
I504285 <bharathhg@gmail.com>
2020-04-13 11:10:26 +0530

    Updated

--- product/src/main/java/com/catalog/product/controller/ProductController.java
+++ product/src/main/java/com/catalog/product/controller/ProductController.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
 
 @Controller
+@RequestMapping(path="/")
 public class ProductController  {
     @Autowired
     private ProductService service;
 
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String welcomePage(){
 
         return "index";
