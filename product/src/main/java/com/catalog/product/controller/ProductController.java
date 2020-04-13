c5db7e59b5981f063f6864693657b0259fee1bb9
I504285 <bharathhg@gmail.com>
2020-04-13 11:58:22 +0530

    Updated

--- product/src/main/java/com/catalog/product/controller/ProductController.java
+++ product/src/main/java/com/catalog/product/controller/ProductController.java
@@ -22,7 +22,7 @@ public class ProductController  {
         List<Product> listProducts=service.listAll();
         model.addAttribute("listProducts",listProducts);
 
-        return "products";
+        return "index";
 
     }
 
