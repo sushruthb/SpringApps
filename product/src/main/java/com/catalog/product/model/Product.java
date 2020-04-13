6e020dcd12cd8ecc0316eb85dc4efb11da87f22b
I504285 <bharathhg@gmail.com>
2020-04-13 10:52:33 +0530

    Updated

--- product/src/main/java/com/catalog/product/model/Product.java
+++ product/src/main/java/com/catalog/product/model/Product.java
@@ -12,12 +12,6 @@ public class Product {
     private String madein;
     private float price;
 
-    public Product(String name, String brand, String madein, float price) {
-        this.name = name;
-        this.brand = brand;
-        this.madein = madein;
-        this.price = price;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
