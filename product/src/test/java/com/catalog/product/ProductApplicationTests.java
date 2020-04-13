1e422cb21cc5e0ef4b6fcd4bf4658d8a12a0f06d
I504285 <bharathhg@gmail.com>
2020-04-13 10:37:56 +0530

    Updated

--- product/src/test/java/com/catalog/product/ProductApplicationTests.java
+++ product/src/test/java/com/catalog/product/ProductApplicationTests.java
@@ -3,7 +3,7 @@ package com.catalog.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ProductApplicationTests {
 
 	@Test
