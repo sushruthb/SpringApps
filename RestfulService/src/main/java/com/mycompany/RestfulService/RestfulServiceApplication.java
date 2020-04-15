0d9f44f8a56474483accaa405b6b1a11f71ce1db
I504285 <bharathhg@gmail.com>
2020-04-15 21:24:34 +0530

    Updates

---
+++ RestfulService/src/main/java/com/mycompany/RestfulService/RestfulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.RestfulService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulServiceApplication.class, args);
+	}
+}
---
+++ RestfulService/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(10001, sysdate(), 'Sushruth');
+insert into user values(10002, sysdate(), 'Bharath');
+insert into user values(10003, sysdate(), 'Kavitha');
+insert into post values(11001, 'My First Post', 10001);
+insert into post values(11002, 'My Second Post', 10001);
