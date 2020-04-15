df2be883d521142987eb5d2e171ce793cfd50ffe
I504285 <bharathhg@gmail.com>
2020-04-15 21:52:59 +0530

    updates

--- product/src/main/java/com/catalog/product/controller/ProductController.java
+++ product/src/main/java/com/catalog/product/controller/ProductController.java
@@ -5,10 +5,12 @@ import com.catalog.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -38,9 +40,11 @@ public class ProductController  {
     }
 
     @PostMapping("/save")
-    public String saveProduct(@ModelAttribute("product") Product product) {
-        service.save(product);
-
+    public String saveProduct(@ModelAttribute("product") @Valid Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            service.save(product);
+            return "redirect:/new_product";
+        }
         return "redirect:/products";
     }
 
