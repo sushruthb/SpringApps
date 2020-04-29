53d8dbb3a774981b34985538d44341d62f34a6a4
I504285 <bharathhg@gmail.com>
2020-04-29 20:40:28 +0530

    Added vue project

---
+++ meal-prep/src/components/AppNavigation.vue
@@ -0,0 +1,14 @@
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Meal Prep',
+            drawer: false,
+            items: [
+                { title: 'Menu' },
+                { title: 'Sign In' },
+                { title: 'Join' }
+            ]
+        };
+    }
+};
