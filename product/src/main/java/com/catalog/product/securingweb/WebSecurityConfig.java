5b49d94bd1a82bb486cc96b797399b8aa1a7f994
I504285 <bharathhg@gmail.com>
2020-05-02 15:41:28 +0530

    Updated

--- product/src/main/java/com/catalog/product/securingweb/WebSecurityConfig.java
+++ product/src/main/java/com/catalog/product/securingweb/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/login","/products","/edit_product").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
