2c18f779ac853b072bfd176fe2de45ce6caef737
I504285 <bharathhg@gmail.com>
2020-05-02 15:20:59 +0530

    Update

--- product/src/main/java/com/catalog/product/securingweb/WebSecurityConfig.java
+++ product/src/main/java/com/catalog/product/securingweb/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/")
                 .permitAll()
                 .and()
                 .logout()
