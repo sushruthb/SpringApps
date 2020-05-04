0496f7e42186515f48df77900da936ca1780a948
Sushruth Bharath HG <bharathhg@gmail.com>
2020-05-04 09:01:40 +0530

    updated

---
+++ springvw/src/main/java/com/springvue/springvw/SpringvwApplication.java
@@ -0,0 +1,13 @@
+package com.springvue.springvw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvwApplication.class, args);
+	}
+
+}
