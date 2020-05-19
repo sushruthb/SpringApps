18c66c6f4124778471dccc3212cf840192cbac07
I504285 <bharathhg@gmail.com>
2020-05-19 14:57:07 +0530

    updated

--- withjs/src/main/java/com/ui/withjs/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ui.withjs.repository;
-
-import com.ui.withjs.model.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
