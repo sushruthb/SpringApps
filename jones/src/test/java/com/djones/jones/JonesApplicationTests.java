194b99a02b5d0ca4ae5d03159af130cf0ab7dff3
I504285 <bharathhg@gmail.com>
2020-05-19 14:47:35 +0530

    Updated

---
+++ jones/src/test/java/com/djones/jones/JonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djones.jones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- restservice/src/main/java/com/spring/restservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.restservice.repository;
-
-import com.spring.restservice.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
