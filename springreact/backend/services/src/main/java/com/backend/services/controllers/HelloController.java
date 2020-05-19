95ea1c99693b213afec7bdad52b670e92730688a
I504285 <bharathhg@gmail.com>
2020-05-19 14:25:41 +0530

    Updated

---
+++ springreact/backend/services/src/main/java/com/backend/services/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.backend.services.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String Hello(){
+        return "Hello";
+    }
+}
