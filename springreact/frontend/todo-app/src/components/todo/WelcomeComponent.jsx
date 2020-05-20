f22767768a6243acd2ac94fdd7c4457f6fc17333
I504285 <bharathhg@gmail.com>
2020-05-20 17:53:45 +0530

    Updated

--- springreact/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ springreact/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -3,6 +3,11 @@ import React, { Component } from "react"
 import {Link} from 'react-router-dom';
 
 class WelcomeComponent extends Component{
+
+    constructor(props){
+        super(props)
+        this.retrieveWelcomeMessage=this.retrieveWelcomeMessage.bind    (this)
+    }
     render(){
         return (
 <>
@@ -12,8 +17,17 @@ class WelcomeComponent extends Component{
                 You can manage your todos <Link to='/todos'>here</Link>
                 
             </div>
+            <div className="Container">
+                Click here to get customized welcome message
+                <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+            </div>
+
             </>
         )
+    }
+
+    retrieveWelcomeMessage(){
+        console.log("Button clicked")
     }
 
 }
