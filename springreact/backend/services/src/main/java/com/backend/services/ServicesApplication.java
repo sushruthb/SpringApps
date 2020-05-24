b21128e3fc5cd4e99e5013de284b7050a6de0808
I504285 <bharathhg@gmail.com>
2020-05-24 19:11:34 +0530

    Updated:

--- springreact/backend/services/src/main/java/com/backend/services/ServicesApplication.java
+++ springreact/backend/services/src/main/java/com/backend/services/ServicesApplication.java
@@ -2,6 +2,10 @@ package com.backend.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ServicesApplication {
@@ -10,4 +14,15 @@ public class ServicesApplication {
 		SpringApplication.run(ServicesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+
+
+			}
+		};
+	}
 }
