8eb3a26497314271184c3255808037fff872dac1
I504285 <bharathhg@gmail.com>
2020-05-28 10:32:08 +0530

    Updated

--- springreact/backend/services/src/main/java/com/backend/services/controllers/TodoController.java
+++ springreact/backend/services/src/main/java/com/backend/services/controllers/TodoController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
 @CrossOrigin(origins = "*")
+@RestController
 public class TodoController {
 
     @Autowired
