a825a9f19ef3476ddc97a80e73ba9632476b439f
I504285 <bharathhg@gmail.com>
2020-06-04 16:44:14 +0530

    Updated

--- springreact/backend/services/src/main/java/com/backend/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ springreact/backend/services/src/main/java/com/backend/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,13 +1,9 @@
 package com.backend.basic.auth;
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.HttpMethod;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
