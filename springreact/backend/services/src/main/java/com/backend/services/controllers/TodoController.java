1b504356d8cba5618816bbe5eb8d84f47d17207c
I504285 <bharathhg@gmail.com>
2020-06-04 16:45:56 +0530

    Updated

--- springreact/backend/services/src/main/java/com/backend/services/controllers/TodoController.java
+++ springreact/backend/services/src/main/java/com/backend/services/controllers/TodoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-@CrossOrigin(origins = "*",allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 @RestController
 public class TodoController {
 
