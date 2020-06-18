cb3f04198a32a5b3196e9cfb76ee882a57484ee9
I504285 <bharathhg@gmail.com>
2020-06-18 19:54:17 +0530

    Updated

--- springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/basic/auth/BasicAuthenticationController.java
+++ springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -3,6 +3,7 @@ package com.backend.rest.services.restfulwebservices.basic.auth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -14,5 +15,10 @@ public class BasicAuthenticationController {
     public AuthenticationBean helloWorldBean() {
         //throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
         return new AuthenticationBean("You are authenticated");
+    }
+
+    @GetMapping(path = "/hello-world-bean/")
+    public String hello(@PathVariable String name){
+        return("Hello " + name);
     }
 }
