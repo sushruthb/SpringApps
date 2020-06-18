3ce7218c46731bc19770181a3cf9aa0b3fec15a8
I504285 <bharathhg@gmail.com>
2020-06-18 16:44:49 +0530

    updated

--- springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/auth/SpringSecurityConfigurationBasicAuth.java
+++ springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,4 +1,4 @@
-package com.backend.rest.services.restfulwebservices.basic.auth;
+package com.backend.rest.services.restfulwebservices.auth;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
