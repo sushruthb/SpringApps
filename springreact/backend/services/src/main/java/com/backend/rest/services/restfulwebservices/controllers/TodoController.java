0b2caccaad53f3d31cd52e5f4159329ead248e6b
I504285 <bharathhg@gmail.com>
2020-06-23 16:12:32 +0530

    Updated

--- springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/controllers/TodoController.java
+++ springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/controllers/TodoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"$cross.origins"})
 @RestController
 public class TodoController {
 
