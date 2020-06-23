539fd14d5d81dee201188f690f2591a4c0f48def
I504285 <bharathhg@gmail.com>
2020-06-23 16:07:22 +0530

    Added Todo Repository

--- springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/services/TodoService.java
+++ springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/services/TodoService.java
@@ -1,6 +1,8 @@
 package com.backend.rest.services.restfulwebservices.services;
 
 import com.backend.rest.services.restfulwebservices.model.Todo;
+import com.backend.rest.services.restfulwebservices.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,14 +11,18 @@ import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    TodoRepository repo;
     private static List<Todo> todos=new ArrayList();
     private static long idCounter=0;
+/*
 
     static{
         todos.add(new Todo(++idCounter,"SpringFullStack","ReactJS",new Date(),false));
         todos.add(new Todo(++idCounter,"SpringFullStack","Spring with ReactJS",new Date(),false));
         todos.add(new Todo(++idCounter,"SpringFullStack","React,ReactJS & MySql",new Date(),false));
     }
+*/
 
     public List<Todo> findAll(){
         return todos;
