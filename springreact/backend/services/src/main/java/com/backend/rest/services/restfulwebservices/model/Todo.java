7defb2602c7be2f478038443fcc06f4475d7a77d
I504285 <bharathhg@gmail.com>
2020-06-23 20:06:28 +0530

    updated

--- springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/model/Todo.java
+++ springreact/backend/services/src/main/java/com/backend/rest/services/restfulwebservices/model/Todo.java
@@ -2,6 +2,7 @@ package com.backend.rest.services.restfulwebservices.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 import java.util.Objects;
@@ -9,7 +10,7 @@ import java.util.Objects;
 @Entity
 public class Todo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String username;
