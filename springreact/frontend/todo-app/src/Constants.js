1f21d3ea188ec6e852f880cd397989aa630efeab
I504285 <bharathhg@gmail.com>
2020-06-23 17:29:41 +0530

    Updated

---
+++ springreact/frontend/todo-app/src/Constants.js
@@ -0,0 +1,2 @@
+export const API_URL='http://10.76.110.207:6262'
+export const JPA_API_URL='http://10.76.110.207:6262/jpa'
--- springreact/frontend/todo-app/src/api/todo/TodoDataService.js
+++ springreact/frontend/todo-app/src/api/todo/TodoDataService.js
@@ -1,39 +1,28 @@
 import axios from 'axios'
-import API_URL from '../../Constants.js'
+import {API_URL,JPA_API_URL} from '../../Constants.js'
 class TodoDataService{
+
     retrieveAllTodos(name){
         console.log('Executed ListTodos Service')
-        return axios.get(`${API_URL}/users/${name}/todos`);
-
-        
+        return axios.get(`${JPA_API_URL}/users/${name}/todos`);
     }
 
     retrieveTodo(name,id){
         console.log('Executed ListTodo Service')
         return axios.get(`${API_URL}/users/${name}/todos/${id}`);
-
-        
     }
     deleteTodo(name,id){
         console.log('Executed DeleteTodo Service')
         return axios.delete(`${API_URL}/users/${name}/todos/${id}`);
-
-        
     }
     updateTodo(name,id,todo){
         console.log('Executed UpdateTodo Service')
         return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
-
-        
     }
     createTodo(name,todo){
-        console.log('Executed UpdateTodo Service')
+        console.log('Executed CreateTodo Service')
         return axios.post(`${API_URL}/users/${name}/todos/`, todo);
-
-        
     }
-
-
 }
 
 export default new TodoDataService()
